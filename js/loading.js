01fbee8dc9c252d41a0049fcf4bdb08373bcc9d0
Mr_IK <34494629+Mr-IK@users.noreply.github.com>
2019-07-06 21:33:59 +0900

    Add files via upload

--- js/loading.js
+++ js/loading.js
@@ -1,12 +1,28 @@
+var flatcolor = "true";
 $(window).on('load',function(){
   // 消えるタイミングはお好みで
   $('#loading').delay(1000).fadeOut(900);
   $('#loader-bg').delay(2000).fadeOut(900);
-  setTimeout('loadBar()',3000);
-  setTimeout('loadMain()',4000);
+  flatcolor = $.cookie('colortype');
+  if (typeof a === "undefined") {
+    //警告画面を出す
+    setTimeout('loadWarning()',2000);
+    flatcolor = true;
+    return;
+  }
+  $('#warning-bg').fadeOut(1);
+  if(flatcolor != 'true'){
+    setTimeout('changeflat()',2000);
+    setTimeout('loadBar()',3000);
+    setTimeout('loadMain()',4000);
+  }else{
+    setTimeout('loadBar()',3000);
+    setTimeout('loadMain()',4000);
+  }
 });
 
 $(document).ready(function () {
+  $.cookie('colortype','false',{ expires: 7, path: '/'  });
   $('#togglebutton').on('click',function(){
     if(flatcolor){
       setTimeout('unloadBar()',100);
@@ -15,6 +31,7 @@ $(document).ready(function () {
       setTimeout('loadMain()',2000);
       setTimeout('loadBar()',2500);
       flatcolor = false;
+      $.cookie('colortype','false',{ expires: 7, path: '/'  });
     }else{
       setTimeout('unloadBar()',100);
       setTimeout('unloadMain()',200);
@@ -22,21 +39,65 @@ $(document).ready(function () {
       setTimeout('loadMain()',2000);
       setTimeout('loadBar()',2500);
       flatcolor = true;
+      $.cookie('colortype','true',{ expires: 7, path: '/'  });
+    }
+  });
+  $('#close').on('click',function(){
+    if(!$("#switch").prop('checked')){
+      setTimeout('unloadWarning()',1);
+      setTimeout('loadflat()',1);
+      setTimeout('loadMain()',1000);
+      setTimeout('loadBar()',1500);
+      flatcolor = false;
+      $.cookie('colortype','false',{ expires: 7, path: '/'  });
+    }else{
+      setTimeout('unloadWarning()',1);
+      setTimeout('loadCool()',1);
+      setTimeout('loadMain()',1000);
+      setTimeout('loadBar()',1500);
+      flatcolor = true;
+      $.cookie('colortype','true',{ expires: 7, path: '/'  });
     }
   });
 });
-
-var flatcolor = true;
 
 function unloadBar(){
   $('#nav').removeClass("nav-load");
   $('#nav').addClass("nav-unload");
 }
 
+function loadWarning(){
+  $('#warning-bg').addClass("main-load");
+}
+
+function unloadWarning(){
+  $('#warning-bg').removeClass("main-load");
+  $('#warning-bg').addClass("warning-unload-ani");
+  setTimeout('byeWarning()',700);
+}
+
+function byeWarning(){
+  $('#warning-bg').addClass("warning-unload");
+}
+
 function changeCool(){
   $('body').removeClass("body-color-flat");
   $('.nav-button').removeClass("btn-color-flat");
   $('body').addClass("body-color-cool");
+  $('.nav-button').addClass("btn-color-cool");
+}
+
+function loadflat(){
+  $('body').removeClass("body-color-cool");
+  $('.nav-button').removeClass("btn-color-cool");
+  $('body').addClass("body-flat");
+  $('.nav-button').addClass("btn-color-flat");
+}
+
+function loadCool(){
+  $('body').removeClass("body-color-flat");
+  $('.nav-button').removeClass("btn-color-flat");
+  $('body').addClass("body-cool");
   $('.nav-button').addClass("btn-color-cool");
 }
 
